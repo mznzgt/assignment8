2795429497040e94fef0998b6d569d6cdfb55170
yl606 <luyj0812@gmail.com>
2018-10-06 08:33:54 +1300

    Set the Game Rank page

--- app/src/main/java/com/example/a8project/rank.java
+++ app/src/main/java/com/example/a8project/rank.java
@@ -1,8 +1,10 @@
 package com.example.a8project;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class rank extends AppCompatActivity {
@@ -14,5 +16,13 @@ public class rank extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public void playAgain(View view){
+        startActivity(new Intent(getApplicationContext(),gameView.class));
+    }
+
+    public void returnToMain(View view){
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
     }
 }
