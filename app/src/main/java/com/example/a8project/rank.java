47580d8ea58e146999f51556ce7b4828caf43b79
yl606 <luyj0812@gmail.com>
2018-10-06 22:15:07 +1300

    Implement rank class and get data from result class

--- app/src/main/java/com/example/a8project/rank.java
+++ app/src/main/java/com/example/a8project/rank.java
@@ -6,9 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class rank extends AppCompatActivity {
-
+    result r = new result();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +17,18 @@ public class rank extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        TextView first = (TextView)findViewById(R.id.first);
+        TextView second = (TextView)findViewById(R.id.second);
+        TextView third = (TextView)findViewById(R.id.third);
+        TextView fourth = (TextView)findViewById(R.id.fourth);
+        TextView fifth = (TextView)findViewById(R.id.fifth);
+
+        first.setText(String.valueOf(r.returnFirst()));
+        second.setText(String.valueOf(r.returnSecond()));
+        third.setText(String.valueOf(r.returnThird()));
+        fourth.setText(String.valueOf(r.returnFourth()));
+        fifth.setText(String.valueOf(r.returnFifth()));
     }
 
     public void playAgain(View view){
